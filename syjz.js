0c0ed5c23f5cf24756eccfa585a0de58e90888f6
WeiRen0210 <76566404+WeiRen0210@users.noreply.github.com>
2020-12-25 01:58:09 +0800

    Update syjz.js

--- syjz.js
+++ syjz.js
@@ -6,7 +6,16 @@ var url = $request.url;
 const path1 = "/account/detail";
 if (url.indexOf(path1) != -1) {
 	let obj = JSON.parse(body);
-	obj.data.vip = {"status":1,"days":999};
+	obj.data.vip = "vip":{"status": 1,
+    "finish_date": "2026-06-06 06:06:06",
+    "finish_date_ios": "2026.06.06",
+    "pre_date": "2020-06-10 16:28:58",
+    "days": 2019,
+    "last_buy_date": "2020-06-11 03:17:09",
+    "auto_buy": 0,
+    "buy_status": 1,
+    "id": 901828,
+    "pre_status": 1}
 	body = JSON.stringify(obj);
  }
 $done({body});
