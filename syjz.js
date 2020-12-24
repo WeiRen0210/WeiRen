268e47d939b33b16f47f6c13b35c7094c882362c
WeiRen0210 <76566404+WeiRen0210@users.noreply.github.com>
2020-12-25 01:12:55 +0800

    Update syjz.js

--- syjz.js
+++ syjz.js
@@ -6,7 +6,7 @@ var url = $request.url;
 const path1 = "/account/detail";
 if (url.indexOf(path1) != -1) {
 	let obj = JSON.parse(body);
-	obj.data.vip = "isvip": {"status":1,"days":999};
+	obj.data.vip = {"isvip": "status":1,"days":999};
 	body = JSON.stringify(obj);
  }
 $done({body});
