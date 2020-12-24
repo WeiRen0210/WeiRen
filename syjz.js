56b94fc9d2cc2dab0720904d0e779359c9b13962
WeiRen0210 <76566404+WeiRen0210@users.noreply.github.com>
2020-12-25 00:57:41 +0800

    Create syjz.js

---
+++ syjz.js
@@ -0,0 +1,14 @@
+# WeiRen
+自用的脚本存放地
+/*
+Unlocks by WeiRen、Lao高
+*/
+var body = $response.body;
+var url = $request.url;
+const path1 = "/account/detail";
+if (url.indexOf(path1) != -1) {
+	let obj = JSON.parse(body);
+	obj.data.vip = "isvip": {"status":1,"days":999};
+	body = JSON.stringify(obj);
+ }
+$done({body});
